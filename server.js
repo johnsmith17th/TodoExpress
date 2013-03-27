fc2b2ad178aa1a69c962444dfd853e1309b202a7
John Smith <john.smith.17th@gmail.com>
2013-03-27 21:52:15 +0800

    Basic framework of application server

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
-var http = require('http');
+var express = require('express'),
+    routes = require('./routes');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+var app = module.exports = express();
+
+// Configuration
+
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    //app.use(express.cookieParser());
+    //app.use(express.session({ secret: 'restcook-session-secret' }));
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function () {
+    app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.post('/item', routes.postItem);
+
+app.listen(8080);
+console.log("TodoExpress server listening on port %d in %s mode", 8080, app.settings.env);
